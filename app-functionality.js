2e432a26e1b64b4d340ce1760874404d8c7e2539
Asik <asikhossain592445@gmail.com>
2025-08-27 15:40:23 +0600

    updated design and partially added functionality

---
+++ app-functionality.js
@@ -0,0 +1,12 @@
+// Adding The Call Button Functionality
+
+document.getElementById('national-em-call')
+    .addEventListener('click', function(){
+        const getCoinText = parseInt(document.getElementById('coin-life').innerText);
+
+        // Adding The Life Remaining Check and Call Functionality
+        const getServiceTitle = document.getElementsById('national-em-call').innerText;
+        if(getCoinText != 0) {
+            alert()
+        }
+    })
